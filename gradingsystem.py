5c115c385a0ebb8d0de2b5e2e29e806790c26d23
AusafAnsari <mr.lol1620074@gmail.com>
2025-06-18 12:04:27 +0530

    Add files via upload

--- gradingsystem.py
+++ gradingsystem.py
@@ -1,14 +1,28 @@
+import firebase_admin
+from firebase_admin import credentials ,db
+cred = credentials.Certificate("C:/Users/adminb/Desktop/python/new-python-b010f-firebase-adminsdk-fbsvc-e4e0096baf.json")
+if not firebase_admin._apps:
+    firebase_admin.initialize_app (cred,{"databaseURL":"https://new-python-b010f-default-rtdb.firebaseio.com/"})
+
 totalmarks=0
 for i in range(5):
-    marks=int(input(f"enter values{i}"))
+    marks=int(input(f"enter values of {i} "))
     totalmarks+=marks #every input gets added
     avg=totalmarks/5  #avg is being taken
 if avg>=90:
-    print("you got A+! Excelent!")#average is being used
+    z="you got A+! Excelent!"#average is being used
 elif avg>=80:
-    print("You got A! good job!")
+    z="You got A! good job!"
 elif avg>=60:
-    print("You got B! keep it up and try harder next time!")#we can also add c and d
+    z="You got B! keep it up and try harder next time!"#we can also add c and d
 else:#less than 50
-    print("F!!")#more grading sytems can be added
+    z="F!!"#more grading sytems can be added
 print(avg)
+print(z)
+
+ref=db.reference('Ansari')
+ref.push({
+"grade":z,
+"average":avg
+}  )
+print("data sent successfull")
